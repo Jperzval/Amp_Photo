c02473d5c7fb759fa4f3f52490fe80f319737b39
greggnicholas <greggnicholas@pursuit.org>
2021-05-12 12:57:13 -0400

    Save item to datastore

--- app/src/main/java/com/gjm/amp_photo/MainActivity.java
+++ app/src/main/java/com/gjm/amp_photo/MainActivity.java
@@ -8,6 +8,7 @@ import android.util.Log;
 import com.amplifyframework.AmplifyException;
 import com.amplifyframework.core.Amplify;
 import com.amplifyframework.datastore.AWSDataStorePlugin;
+import com.amplifyframework.datastore.generated.model.User;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,5 +16,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        User item = User.builder()
+                .name("Build Android application")
+                .build();
+
+        Amplify.DataStore.save(item,
+                success -> Log.i("Tutorial", "Saved item: " + success.item().getName()),
+                error -> Log.e("Tutorial", "Could not save item to DataStore", error)
+        );
     }
 }
