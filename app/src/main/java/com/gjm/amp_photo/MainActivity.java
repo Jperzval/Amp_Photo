6625699c43b2095e0b022f61a5d233f713b7802a
greggnicholas <greggnicholas@pursuit.org>
2021-05-12 13:46:44 -0400

    Query photo

--- app/src/main/java/com/gjm/amp_photo/MainActivity.java
+++ app/src/main/java/com/gjm/amp_photo/MainActivity.java
@@ -8,7 +8,10 @@ import android.util.Log;
 import com.amplifyframework.AmplifyException;
 import com.amplifyframework.core.Amplify;
 import com.amplifyframework.datastore.AWSDataStorePlugin;
+import com.amplifyframework.datastore.generated.model.Photos;
 import com.amplifyframework.datastore.generated.model.User;
+
+import java.util.Iterator;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,13 +20,29 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        User item = User.builder()
-                .name("Build Android application")
-                .build();
+//        User item = User.builder()
+//                .name("Build Android application")
+////                .priority(Priority.HIGH)
+//                .build();
+//
+//        Amplify.DataStore.save(item,
+//                success -> Log.i("Tutorial", "Saved item: " + success.item().getName()),
+//                error -> Log.e("Tutorial", "Could not save item to DataStore", error)
+//        );
 
-        Amplify.DataStore.save(item,
-                success -> Log.i("Tutorial", "Saved item: " + success.item().getName()),
-                error -> Log.e("Tutorial", "Could not save item to DataStore", error)
+        Amplify.DataStore.query(Photos.class,
+                (Iterator<Photos> photos) -> {
+                    while (photos.hasNext()) {
+                        Photos photo = photos.next();
+
+                        Log.i("Tutorial", "==== Todo ====");
+                        if (photo.getPriority() != null) {
+                            Log.i("Tutorial", "Priority: " + photo.getPriority().toString());
+                        }
+
+                    }
+                },
+                failure -> Log.e("Tutorial", "Could not query DataStore", failure)
         );
     }
 }
