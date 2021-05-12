b4f042c87ed3001d73d2d2905e092e75cdb4a74b
Jperzval <perezvalle.jes@gmail.com>
2021-05-12 15:30:17 -0400

    appsync init proccess

--- app/src/main/java/com/gjm/amp_photo/MainActivity.java
+++ app/src/main/java/com/gjm/amp_photo/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.util.Log;
 
 import com.amplifyframework.AmplifyException;
+import com.amplifyframework.api.aws.AWSApiPlugin;
 import com.amplifyframework.core.Amplify;
 import com.amplifyframework.datastore.AWSDataStorePlugin;
 import com.amplifyframework.datastore.generated.model.Photos;
@@ -20,29 +21,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        User item = User.builder()
-//                .name("Build Android application")
-////                .priority(Priority.HIGH)
-//                .build();
-//
-//        Amplify.DataStore.save(item,
-//                success -> Log.i("Tutorial", "Saved item: " + success.item().getName()),
-//                error -> Log.e("Tutorial", "Could not save item to DataStore", error)
-//        );
+        try {
+            Amplify.addPlugin(new AWSApiPlugin());
+            Amplify.addPlugin(new AWSDataStorePlugin());
+            Amplify.configure(getApplicationContext());
 
-        Amplify.DataStore.query(Photos.class,
-                (Iterator<Photos> photos) -> {
-                    while (photos.hasNext()) {
-                        Photos photo = photos.next();
+            Log.i("Tutorial", "Initialized Amplify");
+        } catch (AmplifyException failure) {
+            Log.e("Tutorial", "Could not initialize Amplify", failure);
+        }
 
-                        Log.i("Tutorial", "==== Todo ====");
-                        if (photo.getPriority() != null) {
-                            Log.i("Tutorial", "Priority: " + photo.getPriority().toString());
-                        }
-
-                    }
-                },
-                failure -> Log.e("Tutorial", "Could not query DataStore", failure)
+        Amplify.DataStore.observe(Photos.class,
+                started -> Log.i("Tutorial", "Observation began."),
+                change -> Log.i("Tutorial", change.item().toString()),
+                failure -> Log.e("Tutorial", "Observation failed.", failure),
+                () -> Log.i("Tutorial", "Observation complete.")
         );
     }
 }
